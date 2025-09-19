89022c2e416b2b0e619ea349d0dda235b45e7922
weizheIP <2106405330@qq.com>
2025-09-19 11:41:47 +0800

    feat branch3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ int main(int argc, char ** argv)
 	std::cout << "master hello!" << std::endl;
 	std::cout << "branch1 hello!" << std::endl;
 
+	int i = 1;
+	int j = 2;
+	int k = i + j;
 	std::cout << "hello branch2!" << std::endl;
 	return 0;
+
 
 }
