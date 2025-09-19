e9d26fd31a6821139c9cfb89bbf0a86d05e02d49
weizheIP <2106405330@qq.com>
2025-09-19 11:24:10 +0800

    add branch2.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main(int argc, char ** argv)
 {
 	std::cout << "master hello!" << std::endl;
 	std::cout << "branch1 hello!" << std::endl;
+	std::cout << "branch2 hello!" << std::endl;
 	return 0;
 
 }
